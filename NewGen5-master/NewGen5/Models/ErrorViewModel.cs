5cd1ed38ad65e82d8699e3fac1d8bf896bccbca6
dane97coetzee <39653481+dane97coetzee@users.noreply.github.com>
2018-05-29 11:31:57 +0200

    Completed backup and restore screens

---
+++ NewGen5-master/NewGen5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NewGen5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
